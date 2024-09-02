95c20e523ca57d79cfa1b32c061b699badf31ec4
Vilém Zouhar <vilem.zouhar@gmail.com>
2024-09-02 22:37:52 +0200

    prepare configs

---
+++ vilem/scripts/utils.sh
@@ -0,0 +1,49 @@
+function get_config() {
+    mkdir -p models
+
+    TRAIN_DATA=$(realpath data/csv/train.csv)
+    DEV_DATA=$(realpath data/csv/train_head.csv)
+    CHECKPOINT_DIR=$(realpath "models/")
+    CHECKPOINT_FILENAME=$1
+    ENCODER_MODEL=$2
+    PRETRAINED_MODEL=$3
+    
+    # should prevent collisions
+    mkdir -p tmp
+    TMP_CONFIG_DIR=$(mktemp -d -p 'tmp/')
+    cp configs/* ${TMP_CONFIG_DIR}
+
+    cat configs/model.yaml \
+        | sed "s|TRAIN_DATA_PATH|${TRAIN_DATA}|" \
+        | sed "s|DEV_DATA_PATH|${DEV_DATA}|" \
+        | sed "s|ENCODER_MODEL|${ENCODER_MODEL}|" \
+        | sed "s|PRETRAINED_MODEL|${PRETRAINED_MODEL}|" \
+        > ${TMP_CONFIG_DIR}/model.yaml
+    cat configs/model_checkpoint.yaml \
+        | sed "s|CHECKPOINT_DIR|${CHECKPOINT_DIR}|" \
+        | sed "s|CHECKPOINT_FILENAME|${CHECKPOINT_FILENAME}|" \
+        > ${TMP_CONFIG_DIR}/model_checkpoint.yaml
+    echo ${TMP_CONFIG_DIR}/model.yaml
+}
+
+function sbatch_gpu() {
+    JOB_NAME=$1;
+    JOB_WRAP=$2;
+    mkdir -p logs
+
+    sbatch \
+        -J $JOB_NAME --output=logs/%x.out --error=logs/%x.err \
+        --gpus=1 --gres=gpumem:22g \
+        --ntasks-per-node=1 \
+        --cpus-per-task=6 \
+        --mem-per-cpu=8G --time=1-0 \
+        --wrap="$JOB_WRAP";
+}
+
+function local_gpu() {
+    JOB_NAME=$1;
+    JOB_WRAP=$2;
+    mkdir -p logs
+
+    $JOB_WRAP 1> logs/${JOB_NAME}.out 2> logs/${JOB_NAME}.err
+}
