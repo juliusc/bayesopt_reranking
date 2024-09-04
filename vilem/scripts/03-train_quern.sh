e26fd19a07c573293b5dd4e64ba6c8a92bd9b0ee
Ondrej Platek <ondrej.platek@seznam.cz>
2024-09-04 09:57:16 +0200

    adapt the rest of training examples to ufal

--- vilem/scripts/03-train_quern.sh
+++ vilem/scripts/03-train_quern.sh
@@ -6,17 +6,18 @@ printf "Running on $HOSTNAME"
 
 if [[ $HOSTNAME = "sol2" ]] ; then
 # ufal cluster start
+
+# WARNING: These commands are blocking ATM so run them in different terminal 
+# Ondra needs to fix --async true setup
 # gpu-python comet-train --cfg $(get_config 'XLM-RoBERTa' 'xlm-roberta-base' 'quern-xlm-roberta-base')
 
-# sbatch_gpu "quern-xlm-roberta-large" "comet-train --cfg $(get_config 'XLM-RoBERTa' 'xlm-roberta-large' 'quern-xlm-roberta-large')"
-#
-# sbatch_gpu "quern-bert-base-multilingual-cased" "comet-train --cfg $(get_config 'BERT' 'bert-base-multilingual-cased' 'quern-bert-base-multilingual-cased')"
-#
-# sbatch_gpu "quern-minilm" "comet-train --cfg $(get_config 'MiniLM' 'microsoft/Multilingual-MiniLM-L12-H384' 'quern-minilm')"
-set -x
+gpu-python COMET/comet/cli/train.py --cfg $(get_config 'XLM-RoBERTa' 'xlm-roberta-large' 'quern-xlm-roberta-large')
+
+gpu-python COMET/comet/cli/train.py --cfg $(get_config 'BERT' 'bert-base-multilingual-cased' 'quern-bert-base-multilingual-cased')
+
 gpu-python COMET/comet/cli/train.py --cfg $(get_config 'MiniLM' 'microsoft/Multilingual-MiniLM-L12-H384' 'quern-minilm')
-#
-# sbatch_gpu "quern-rembert" "comet-train --cfg $(get_config 'RemBERT' 'google/rembert' 'quern-rembert')"
+
+gpu-python COMET/comet/cli/train.py --cfg $(get_config 'RemBERT' 'google/rembert' 'quern-rembert')
 
 # ufal cluster end
 else
