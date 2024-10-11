cea816cc328567b9dfccad5aabb29819dd6f448f
Vilém Zouhar <vilem.zouhar@gmail.com>
2024-10-11 14:57:00 +0200

    qe models runtime benchmarking

--- vilem/scripts/04a-score_comet.py
+++ vilem/scripts/04a-score_comet.py
@@ -2,11 +2,12 @@ import argparse
 from comet import download_model, load_from_checkpoint
 import os
 import json
+import time
 
 args = argparse.ArgumentParser()
 args.add_argument('-m', '--model')
 args.add_argument('-d', '--data', default='data/jsonl/test.jsonl')
-args.add_argument('-o', '--out')
+args.add_argument('-o', '--out', default="/dev/null")
 args.add_argument('-bs', '--batch-size', type=int, default=16)
 args = args.parse_args()
 
@@ -32,7 +33,11 @@ data_comet = [
 ]
 print("Data size:", len(data_comet))
 
+time_start = time.time()
 output = model.predict(data_comet, batch_size=args.batch_size, gpus=1).scores
+time_total = time.time() - time_start
+print(f"Time: {time_total:.2f}s")
+print(f"Time/1000 samples: {time_total/len(data)*1000:.2f}s")
 
 with open(args.out, "w") as f:
     for line, score in zip(data, output):
