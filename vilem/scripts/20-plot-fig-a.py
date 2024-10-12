df93f74ffb9c4412fbe79ca2edb231ae32041db0
Vilém Zouhar <vilem.zouhar@gmail.com>
2024-10-12 16:27:04 +0200

    two new figures

---
+++ vilem/scripts/20-plot-fig-a.py
@@ -0,0 +1,101 @@
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib as mpl
+
+
+COLORS = [
+    "#b93f43", # red
+    "#548f5c", # green
+    "#7f99cc", # light blue
+    "#5c3f81", # purple
+    "#b4ab30", # yellow
+]
+
+# set default line width
+mpl.rcParams["lines.linewidth"] = 2
+
+data = h5py.File('computed/results/all_0.7_1.h5', 'r')
+
+_, axs = plt.subplots(nrows=2, ncols=1, figsize=(4, 6))
+
+TYPE = "score" 
+axs[0].plot(
+    list(data[f"bayesopt_{TYPE}"][10:]),
+    label=f"BayesOpt ({np.average(data[f'bayesopt_{TYPE}'][10:]):.4f})",
+    color=COLORS[0],
+)
+axs[0].plot(
+    list(data[f"hill_climbing_{TYPE}"][10:]),
+    label=f"HillClimbing ({np.average(data[f'hill_climbing_{TYPE}'][10:]):.4f})",
+    color=COLORS[2],
+)
+axs[0].plot(
+    list(data[f"avg_logprob_first_{TYPE}"][10:]),
+    label=f"LogprobAvg ({np.average(data[f'avg_logprob_first_{TYPE}'][10:]):.4f})",
+    color=COLORS[1],
+)
+axs[0].plot(
+    list(data[f"random_deduped_{TYPE}"][10:]),
+    label=f"UniqRandom ({np.average(data[f'random_deduped_{TYPE}'][10:]):.4f})",
+    color=COLORS[3],
+)
+axs[0].plot(
+    list(data[f"sum_logprob_first_{TYPE}"][10:]),
+    label=f"LogprobSum ({np.average(data[f'sum_logprob_first_{TYPE}'][10:]):.4f})",
+    color=COLORS[4],
+)
+
+TYPE = "best_retrieved" 
+axs[1].plot(
+    list(data[f"bayesopt_{TYPE}"][10:]),
+    label=f"BayesOpt ({np.average(data[f'bayesopt_{TYPE}'][10:]):.4f})",
+    color=COLORS[0],
+)
+axs[1].plot(
+    list(data[f"avg_logprob_first_{TYPE}"][10:]),
+    label=f"LogprobAvg ({np.average(data[f'avg_logprob_first_{TYPE}'][10:]):.4f})",
+    color=COLORS[1],
+)
+axs[1].plot(
+    list(data[f"hill_climbing_{TYPE}"][10:]),
+    label=f"HillClimbing ({np.average(data[f'hill_climbing_{TYPE}'][10:]):.4f})",
+    color=COLORS[2],
+)
+axs[1].plot(
+    list(data[f"random_deduped_{TYPE}"][10:]),
+    label=f"UniqRandom ({np.average(data[f'random_deduped_{TYPE}'][10:]):.4f})",
+    color=COLORS[3],
+)
+axs[1].plot(
+    list(data[f"sum_logprob_first_{TYPE}"][10:]),
+    label=f"LogprobSum ({np.average(data[f'sum_logprob_first_{TYPE}'][10:]):.4f})",
+    color=COLORS[4],
+)
+
+axs[0].set_ylabel("Selected candidate score")
+axs[1].set_ylabel("% Selected candidate is top")
+axs[0].set_xlabel("Number of CometKiwi runs")
+axs[1].set_xlabel("Number of CometKiwi runs")
+
+axs[0].set_ylim(0.79, None)
+
+for ax in axs.flatten():
+    ax.spines[["top", "right"]].set_visible(False)
+    ax.set_xticks(
+        np.arange(10, 250, 50)-10,
+        np.arange(10, 250, 50),
+    )
+    ax.legend(
+        edgecolor="white",
+        fancybox=False,
+        handlelength=1,
+        labelspacing=0.3,
+        loc="lower right",
+        bbox_to_anchor=(1.05, 0),
+        framealpha=0,
+    )
+
+plt.tight_layout()
+plt.savefig("figures/results_baselines.pdf")
+plt.show()
---
+++ vilem/scripts/21-plot-fig-b.py
@@ -0,0 +1,58 @@
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+COLORS = [
+    "#b93f43", # red
+    "#548f5c", # green
+    "#7f99cc", # light blue
+    "#5c3f81", # purple
+    "#b4ab30", # yellow
+]
+
+data_1 = h5py.File('computed/results/all_0.7_1.h5', 'r')
+data_2 = h5py.File('computed/results/all_0.7_2.h5', 'r')
+data_5 = h5py.File('computed/results/all_0.7_5.h5', 'r')
+data_10 = h5py.File('computed/results/all_0.7_10.h5', 'r')
+TYPE = "score" 
+
+plt.figure(figsize=(4, 3))
+plt.gca().set_clip_on(False)
+
+for data_i, (data, bs) in enumerate([(data_2, 2), (data_5, 5), (data_10, 10)]):
+    data_x = [i for i,x in enumerate(data[f'bayesopt_{TYPE}']) if x >= 0.1]
+    data_y_bs1 = [x for i,x in enumerate(data_1[f'bayesopt_{TYPE}']) if i in data_x]
+    data_y = [x for i,x in enumerate(data[f'bayesopt_{TYPE}']) if x >= 0.1]
+    #  ({np.average(data_y):.4f})
+    plt.plot(
+        data_x,
+        np.array(data_y_bs1)-np.array(data_y),
+        label=f"BayesOpt batch size {bs}",
+        color=COLORS[0],
+        alpha=1-data_i/3,
+        linewidth=3,
+    )
+
+plt.xlim(-10, 210)
+plt.hlines(0, *plt.xlim(), color="black", linewidth=0.7)
+
+plt.gca().spines[["top", "right", "bottom"]].set_visible(False)
+plt.ylabel("$\Delta$ in selected candidate score\nagainst batch size 1")
+plt.xlabel("Number of CometKiwi runs")
+
+plt.xticks(
+    np.arange(10, 250, 50),
+    np.arange(10, 250, 50),
+)
+plt.legend(
+    edgecolor="white",
+    fancybox=False,
+    framealpha=0,
+    handlelength=1,
+    labelspacing=0.3,
+)
+
+plt.tight_layout()
+plt.savefig("figures/batch_size.pdf")
+plt.show()
